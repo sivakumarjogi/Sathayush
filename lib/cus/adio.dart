b2e71da784bc89464bfcbff7481eb4c7e03a84a5
Nagalakshmi <nagalakshmi.sathayush27@gmail.com>
2024-05-04 11:13:00 -0700

    text field modified

--- lib/cus/adio.dart
+++ lib/cus/adio.dart
@@ -15,7 +15,7 @@ class _AdioState extends State<Adio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar("Page one",const TextStyle(color: Colors.amber,fontSize: 25),bgColor: Colors.green,),
+      appBar: CustomAppBar("Sheet one",const TextStyle(color: Colors.amber,fontSize: 25),bgColor: Colors.green,),
       body: Center(
         child: InkWell(
           onTap: (){
@@ -24,7 +24,7 @@ class _AdioState extends State<Adio> {
           },
           child:  Column(
             children: [
-             const Text("page0"),
+             const Text("Sheet 0"),
               Expanded(child: CustomListView(itemColour: Colors.amber,itemCount: 5,borderColor: Colors.cyan,))
             ],
           ),
